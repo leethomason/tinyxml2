e923c5c4c32db8d50b1d790196d5767dad2ed5f7
Lee Thomason <leethomason@gmail.com>
2012-03-24 13:07:06 -0700

    upped the version.

--- tinyxml2.h
+++ tinyxml2.h
@@ -95,7 +95,7 @@ distribution.
 
 static const int TIXML2_MAJOR_VERSION = 0;
 static const int TIXML2_MINOR_VERSION = 9;
-static const int TIXML2_PATCH_VERSION = 1;
+static const int TIXML2_PATCH_VERSION = 2;
 
 namespace tinyxml2
 {
