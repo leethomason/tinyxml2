c26fdc48635356df0a99145d06c125d2537ecf12
Juan Cardelino <juan.cardelino@gmail.com>
2012-03-31 03:24:06 -0300

    clean up and ordering of the examples

--- examples/example2.cpp
+++ examples/example2.cpp
@@ -5,11 +5,11 @@ using namespace tinyxml2;
 int main (int argc, char * const argv[]) 
 {
    XMLDocument doc;
-   if (doc.LoadFile("demo.xml") == XML_SUCCESS)
+   if (doc.LoadFile("input_example_02.xml") == XML_SUCCESS)
    {
       XMLNode *node = doc.NewElement("foo");
       doc.InsertEndChild(node);
-      doc.SaveFile("demo2.xml");
+      doc.SaveFile("output_example_02.xml");
    }
 	return 0;
 }
