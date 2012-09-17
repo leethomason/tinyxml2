108f7b52ea6770d8b0b4fe7e85f02072fe61e622
sniperbat <sniperbat@gmail.com>
2012-09-17 20:21:12 +0800

    GetErrorStr1 and GetErrorStr2 are not enough to get the information of the error,because at some where the SetError method just set the str1 and str2 to 0( for example line 1579 ). The ErrorID method just return a int, in fact error enumeration itself are good detail description of the error. So, I think the GetErrorIDAsStr is more useful then others.

--- tinyxml2.h
+++ tinyxml2.h
@@ -1190,6 +1190,8 @@ public:
 	bool Error() const { return errorID != XML_NO_ERROR; }
 	/// Return the errorID.
 	int  ErrorID() const { return errorID; }
+	/// Reture the errorID`s as String 
+	const char* GetErrorIDAsStr();
 	/// Return a possibly helpful diagnostic location or string.
 	const char* GetErrorStr1() const { return errorStr1; }
 	/// Return a possibly helpful secondary diagnostic location or string.
