c9752dc99abf2b02fe960f4016426d4fe11aa03a
Martinsh Shaiters <martinsh.shaiters@gmail.com>
2013-01-12 22:43:05 +0200

    Rewrite of setversion.py to write version info to separate files
    setversion.py now writes version configuration files that are consumed via native include mechanisms. It has some basic error checking via exceptions and prints status information.

--- setversion.py
+++ setversion.py
@@ -1,107 +1,58 @@
 # Python program to set the version.
 ##############################################
+import os.path
 
-import re
-import sys
 
-def fileProcess( name, lineFunction ):
-	filestream = open( name, 'r' )
-	if filestream.closed:
-		print( "file " + name + " not open." )
-		return
+def main():
+	version = {}
+	version["major"] = int(raw_input("Major: "))
+	version["minor"] = int(raw_input("Minor: "))
+	version["patch"] = int(raw_input("Patch: "))
 
-	output = ""
-	print( "--- Processing " + name + " ---------" )
-	while 1:
-		line = filestream.readline()
-		if not line: break
-		output += lineFunction( line )
-	filestream.close()
 	
-	if not output: return			# basic error checking
+	cmake_contents = [
+		"set(VERSION_MAJOR {major})\n".format(**version),
+		"set(VERSION_MINOR {minor})\n".format(**version),
+		"set(VERSION_PATCH {patch})\n".format(**version)
+	]
 	
-	print( "Writing file " + name )
-	filestream = open( name, "w" );
-	filestream.write( output );
-	filestream.close()
+	include_contents = [
+		"static const int TIXML2_MAJOR_VERSION = {major};\n".format(**version),
+		"static const int TIXML2_MINOR_VERSION = {minor};\n".format(**version),
+		"static const int TIXML2_PATCH_VERSION = {patch};\n".format(**version)
+	]
+	
+	dox_contents = [
+		"PROJECT_NUMBER = {major}.{minor}.{patch}\n".format(**version)
+	]
 	
 	
-def echoInput( line ):
-	return line
+	config_dir = "config"
+	try:
+		os.mkdir(config_dir)
+	except OSError:
+		pass  # directory already exists
+	
+	print "=> Writting config files with version [{major}.{minor}.{patch}]".format(**version)
+	
+	config_files = []
+	config_files.append({"filepath" : os.path.join(config_dir, "version.cmake"), "contents" : cmake_contents})
+	config_files.append({"filepath" : os.path.join(config_dir, "version.h"), "contents" : include_contents})
+	config_files.append({"filepath" : os.path.join(config_dir, "version.dox"), "contents" : dox_contents})
+	
+	for c in config_files:
+		print "=> Writting '{filepath}'".format(**c)
+		write_config_file(c)
+	
+	print "=> Done"
 
-major = input( "Major: " )
-minor = input( "Minor: " )
-build = input( "Build: " )
-
-print "Setting dox,tinyxml2.h"
-print "Version: " + `major` + "." + `minor` + "." + `build`
-
-#### Write the tinyxml.h ####
-
-def engineRule( line ):
-
-	matchMajor = "static const int TIXML2_MAJOR_VERSION"
-	matchMinor = "static const int TIXML2_MINOR_VERSION"
-	matchBuild = "static const int TIXML2_PATCH_VERSION"
-
-	if line[0:len(matchMajor)] == matchMajor:
-		print "1)tinyxml2.h Major found"
-		return matchMajor + " = " + `major` + ";\n"
-
-	elif line[0:len(matchMinor)] == matchMinor:
-		print "2)tinyxml2.h Minor found"
-		return matchMinor + " = " + `minor` + ";\n"
-
-	elif line[0:len(matchBuild)] == matchBuild:
-		print "3)tinyxml2.h Build found"
-		return matchBuild + " = " + `build` + ";\n"
-
-	else:
-		return line;
-
-fileProcess( "tinyxml2.h", engineRule )
+def write_config_file(config):
+	try:
+		f = open(config["filepath"], 'w')
+		f.writelines(config["contents"])
+	except IOError:
+		print "=! Could not write '{0}'".format(os.path.basename(config["filepath"]))
 
 
-#### Write the dox ####
-
-def doxRule( line ):
-
-	match = "PROJECT_NUMBER"
-
-	if line[0:len( match )] == match:
-		print "dox project found"
-		return "PROJECT_NUMBER = " + `major` + "." + `minor` + "." + `build` + "\n"
-
-	else:
-		return line;
-
-fileProcess( "dox", doxRule )
-
-
-#### Write the CMakeLists.txt ####
-
-def cmakeRule1( line ):
-
-	matchVersion = "set(GENERIC_LIB_VERSION"
-
-	if line[0:len(matchVersion)] == matchVersion:
-		print "1)tinyxml2.h Major found"
-		return matchVersion + " \"" + `major` + "." + `minor` + "." + `build` + "\")" + "\n"
-
-	else:
-		return line;
-
-fileProcess( "CMakeLists.txt", cmakeRule1 )
-
-def cmakeRule2( line ):
-
-	matchSoversion = "set(GENERIC_LIB_SOVERSION"
-
-	if line[0:len(matchSoversion)] == matchSoversion:
-		print "1)tinyxml2.h Major found"
-		return matchSoversion + " \"" + `major` + "\")" + "\n"
-
-	else:
-		return line;
-
-fileProcess( "CMakeLists.txt", cmakeRule2 )
+if __name__ == "__main__":
+	main()
