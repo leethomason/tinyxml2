d31021392a1877a3433c4174bfdd8204690ab23f
Dennis Jenkins <dennis.jenkins.75@gmail.com>
2013-10-11 16:59:49 -0500

    Changed indentation style to match tinyxml2 project.

--- contrib/html5-printer.cpp
+++ contrib/html5-printer.cpp
@@ -1,22 +1,43 @@
-//	g++ -Wall -O2 contrib/html5-printer.cpp -o html5-printer -ltinyxml2
+//  g++ -Wall -O2 contrib/html5-printer.cpp -o html5-printer -ltinyxml2
 
-//	This program demonstrates how to use "tinyxml2" to generate conformant HTML5
-//	by deriving from the "tinyxml2::XMLPrinter" class.
+//  This program demonstrates how to use "tinyxml2" to generate conformant HTML5
+//  by deriving from the "tinyxml2::XMLPrinter" class.
 
-//	http://dev.w3.org/html5/markup/syntax.html
+//  http://dev.w3.org/html5/markup/syntax.html
 
-//	In HTML5, there are 16 so-called "void" elements.  "void elements" NEVER have
-//	inner content (but they MAY have attributes), and are assumed to be self-closing.
-//	An example of a self-closig HTML5 element is "<br/>" (line break)
-//	All other elements are called "non-void" and MUST never self-close.
-//	Examples: "<div class='lolcats'></div>".
+//  In HTML5, there are 16 so-called "void" elements.  "void elements" NEVER have
+//  inner content (but they MAY have attributes), and are assumed to be self-closing.
+//  An example of a self-closig HTML5 element is "<br/>" (line break)
+//  All other elements are called "non-void" and MUST never self-close.
+//  Examples: "<div class='lolcats'></div>".
 
-//	tinyxml2::XMLPrinter will emit _ALL_ XML elements with no inner content as
-//	self-closing.  This behavior produces space-effeceint XML, but incorrect HTML5.
+//  tinyxml2::XMLPrinter will emit _ALL_ XML elements with no inner content as
+//  self-closing.  This behavior produces space-effeceint XML, but incorrect HTML5.
 
-//	Author: Dennis Jenkins,  dennis (dot) jenkins (dot) 75 (at) gmail (dot) com.
-//	License: Same as tinyxml2 (zlib)
-//		This example is a small contribution to the world!  Enjoy it!
+//  Author: Dennis Jenkins,  dennis (dot) jenkins (dot) 75 (at) gmail (dot) com.
+//  License: Same as tinyxml2 (zlib, see below)
+//  This example is a small contribution to the world!  Enjoy it!
+
+/*
+This software is provided 'as-is', without any express or implied
+warranty. In no event will the authors be held liable for any
+damages arising from the use of this software.
+
+Permission is granted to anyone to use this software for any
+purpose, including commercial applications, and to alter it and
+redistribute it freely, subject to the following restrictions:
+
+1. The origin of this software must not be misrepresented; you must
+not claim that you wrote the original software. If you use this
+software in a product, an acknowledgment in the product documentation
+would be appreciated but is not required.
+
+2. Altered source versions must be plainly marked as such, and
+must not be misrepresented as being the original software.
+
+3. This notice may not be removed or altered from any source
+distribution.
+*/
 
 
 #include <tinyxml2.h>
