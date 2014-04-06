6ee53e7d49a4dbfd5c1d7e67fb0228703bbe7309
Lee Thomason <leethomason@gmail.com>
2014-04-06 14:43:37 -0700

    setting the version to 2.1.0

--- tinyxml2.h
+++ tinyxml2.h
@@ -120,8 +120,8 @@ inline int TIXML_SNPRINTF( char* buffer, size_t size, const char* format, ... )
 	http://semver.org/
 */
 static const int TIXML2_MAJOR_VERSION = 2;
-static const int TIXML2_MINOR_VERSION = 0;
-static const int TIXML2_PATCH_VERSION = 2;
+static const int TIXML2_MINOR_VERSION = 1;
+static const int TIXML2_PATCH_VERSION = 0;
 
 namespace tinyxml2
 {
