8a0006ceda31b232374626bc913fce74e265189d
Anton Indrawan <anton.indrawan@gmail.com>
2014-11-20 18:27:07 +0100

    Compile TinyXml2 with the Dinkum C++ of QNX 6.5 and QNX 6.6. Processed review comment from leethomason

--- xmltest.cpp
+++ xmltest.cpp
@@ -23,6 +23,7 @@
 #endif
 
 using namespace tinyxml2;
+using namespace std;
 int gPass = 0;
 int gFail = 0;
 
@@ -296,12 +297,12 @@ int main( int argc, const char ** argv )
 
 	if ( argc > 1 ) {
 		XMLDocument* doc = new XMLDocument();
-		clock_t startTime = std::clock();
+		clock_t startTime = clock();
 		doc->LoadFile( argv[1] );
- 		clock_t loadTime = std::clock();
+ 		clock_t loadTime = clock();
 		int errorID = doc->ErrorID();
 		delete doc; doc = 0;
- 		clock_t deleteTime = std::clock();
+ 		clock_t deleteTime = clock();
 
 		printf( "Test file '%s' loaded. ErrorID=%d\n", argv[1], errorID );
 		if ( !errorID ) {
@@ -1403,7 +1404,7 @@ int main( int argc, const char ** argv )
 #if defined( _MSC_VER )
 		QueryPerformanceCounter( (LARGE_INTEGER*) &start );
 #else
-		clock_t cstart = std::clock();
+		clock_t cstart = clock();
 #endif
 		static const int COUNT = 10;
 		for( int i=0; i<COUNT; ++i ) {
@@ -1413,7 +1414,7 @@ int main( int argc, const char ** argv )
 #if defined( _MSC_VER )
 		QueryPerformanceCounter( (LARGE_INTEGER*) &end );
 #else
-		clock_t cend = std::clock();
+		clock_t cend = clock();
 #endif
 
 		delete [] mem;
