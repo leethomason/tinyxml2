358202c77afedd3d79917930ae489d2d7d2bec74
Philipp Kloke <philipp.kloke@web.de>
2015-07-30 16:02:26 +0200

    Moved include of <cstdarg> to cpp file

--- tinyxml2.cpp
+++ tinyxml2.cpp
@@ -26,8 +26,10 @@ distribution.
 #include <new>		// yes, this one new style header, is in the Android SDK.
 #if defined(ANDROID_NDK) || defined(__QNXNTO__)
 #   include <stddef.h>
+#   include <stdarg.h>
 #else
 #   include <cstddef>
+#   include <cstdarg>
 #endif
 
 #if defined(_MSC_VER) && (_MSC_VER >= 1400 ) && (!defined WINCE)
