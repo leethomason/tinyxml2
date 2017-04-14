803618a2e43d0c333cc238bcbb9b91ce4fe8ba4e
Dmitry Frantsev <Dmitry.Frantsev@gmail.com>
2017-04-14 08:36:14 +0300

    Extra space was removed to pass tests.

--- tinyxml2.cpp
+++ tinyxml2.cpp
@@ -2518,7 +2518,7 @@ void XMLPrinter::CloseElement( bool compactMode )
     const char* name = _stack.Pop();
 
     if ( _elementJustOpened ) {
-        Print( " />" );
+        Print( "/>" );
     }
     else {
         if ( _textDepth < 0 && !compactMode) {
