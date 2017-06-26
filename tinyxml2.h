37bc3aca429f0164adf68c23444540b4a24b5778
Lee Thomason <leethomason@gmail.com>
2017-06-26 16:56:16 -0700

    upping the version to 5.0.1 to fix release vs. source problem

--- tinyxml2.h
+++ tinyxml2.h
@@ -98,7 +98,7 @@ distribution.
 /* Versioning, past 1.0.14:
 	http://semver.org/
 */
-static const int TIXML2_MAJOR_VERSION = 4;
+static const int TIXML2_MAJOR_VERSION = 5;
 static const int TIXML2_MINOR_VERSION = 0;
 static const int TIXML2_PATCH_VERSION = 1;
 
