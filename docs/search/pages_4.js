aca4fb16d001d77b3fbdb2856f17ec945911dc02
Lee Thomason <leethomason@gmail.com>
2017-07-15 19:38:08 -0700

    new docs

--- docs/search/pages_4.js
+++ docs/search/pages_4.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['tinyxml_2d2_20_5b_21_5btravisci_20status_5d_28https_3a_2f_2ftravis_2dci_2eorg_2fleethomason_2ftinyxml2_2esvg_3fbranch_3dmaster_29_5d_28https_3a_2f_2ftravis_2dci_2eorg_2fleethomason_2ftinyxml2_29_20_5b_21_5bappveyor_20status_5d_28https_3a_2f_2fci_2eappveyor_2ecom_2fapi_2fprojects_2fstatus_2fgithub_2fleethomason_2ftinyxml2_3fbranch_3dmaster_26svg_3dtrue_29_5d_28https_3a_2f_2fci_2eappveyor_2ecom_2fproject_2fleethomason_2ftinyxml2_29',['TinyXML-2 [![TravisCI Status](https://travis-ci.org/leethomason/tinyxml2.svg?branch=master)](https://travis-ci.org/leethomason/tinyxml2) [![AppVeyor Status](https://ci.appveyor.com/api/projects/status/github/leethomason/tinyxml2?branch=master&amp;svg=true)](https://ci.appveyor.com/project/leethomason/tinyxml2)',['../index.html',1,'']]]
+  ['tinyxml_2d2',['TinyXML-2',['../index.html',1,'']]]
 ];
