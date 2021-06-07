1dee28e51f9175a31955b9791c74c430fe13dc82
Lee Thomason <leethomason@gmail.com>
2021-06-06 17:10:24 -0700

    setting the version to 9.0.0

--- tinyxml2.h
+++ tinyxml2.h
@@ -99,12 +99,12 @@ distribution.
 /* Versioning, past 1.0.14:
 	http://semver.org/
 */
-static const int TIXML2_MAJOR_VERSION = 8;
-static const int TIXML2_MINOR_VERSION = 1;
+static const int TIXML2_MAJOR_VERSION = 9;
+static const int TIXML2_MINOR_VERSION = 0;
 static const int TIXML2_PATCH_VERSION = 0;
 
-#define TINYXML2_MAJOR_VERSION 8
-#define TINYXML2_MINOR_VERSION 1
+#define TINYXML2_MAJOR_VERSION 9
+#define TINYXML2_MINOR_VERSION 0
 #define TINYXML2_PATCH_VERSION 0
 
 // A fixed element depth limit is problematic. There needs to be a
