d9fb8d34439be452c18b63713e2e0ae0e6028072
jixiaolong <jixiaolong@bilibili.com>
2022-03-03 19:44:45 +0800

    fix: use fseeko64,ftello64 in android when api level >= 24

--- tinyxml2.cpp
+++ tinyxml2.cpp
@@ -104,9 +104,17 @@ distribution.
 	#define TIXML_FSEEK _fseeki64
 	#define TIXML_FTELL _ftelli64
 #elif defined(__APPLE__) || defined(__FreeBSD__) || defined(__OpenBSD__) \
-	|| defined(__NetBSD__) || defined(__DragonFly__) || defined(__ANDROID__)
+	|| defined(__NetBSD__) || defined(__DragonFly__)
 	#define TIXML_FSEEK fseeko
 	#define TIXML_FTELL ftello
+#elif defined(__ANDROID__)
+    #if __ANDROID_API__ > 24
+        #define TIXML_FSEEK fseeko64
+        #define TIXML_FTELL ftello64
+    #else
+        #define TIXML_FSEEK fseeko
+        #define TIXML_FTELL ftello
+    #endif
 #elif defined(__unix__) && defined(__x86_64__)
 	#define TIXML_FSEEK fseeko64
 	#define TIXML_FTELL ftello64
